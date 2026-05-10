978400e51486ac209c177785491823858b018d17
araux5 <alex.samano795@gmail.com>
2026-05-09 22:23:49 -0500

    no timer within levels, leaving that to main game control module

--- GameController/Levels.v
+++ GameController/Levels.v
@@ -1,11 +1,9 @@
-module Levels(levelNum, button_inputs, rng_in, score_increase, score_decrease, mole_location, spike_location, timerEnable, timerReconfig, timerLength ,timerDone, clk, rst);
+module Levels(levelNum, button_inputs, rng_in, score_increase, score_decrease, obj_type_out, obj_location, timer, clk, rst);
     input [1:0] levelNum; 
     input [3:0] button_inputs;
     input [7:0] rng_in;
-    input timerDone, clk, rst;
-    output reg score_increase, score_decrease, timerReconfig;
-    output reg timerEnable;
-    output reg [3:0] timerLength; // used to reconfig timer
+    input timer, clk, rst;
+    output reg score_increase, score_decrease;
     output reg [3:0] obj_location; 
     output reg [3:0] obj_type_out;
 
@@ -26,8 +24,6 @@ module Levels(levelNum, button_inputs, rng_in, score_increase, score_decrease, m
     assign obj_type[3] = rng_in[7];
 
 
-    parameter TIMER_LENGTH1 = 4'd10, TIMER_LENGTH2 = 4'd7, TIMER_LENGTH3 = 4'd5; 
-
     // latch locations and type
     reg [3:0] objLoc_r;
     reg [3:0] objType_r;
@@ -35,13 +31,9 @@ module Levels(levelNum, button_inputs, rng_in, score_increase, score_decrease, m
     always @(posedge clk) begin
             score_increase <= 1'b0;
             score_decrease <= 1'b0;
-            timerReconfig <= 1'b0;
 
         if (rst) begin
-            state <= LEVEL1; 
-            timerEnable <= 1'b0;
-            timerReconfig <= 1'b0;
-            timerLength <= TIMER_LENGTH1; 
+            state <= IDLE; 
             obj_location <= 4'b0000;
             obj_type_out <= 4'b0000;
             objLoc_r <= 4'b0000;
@@ -51,22 +43,18 @@ module Levels(levelNum, button_inputs, rng_in, score_increase, score_decrease, m
         else begin 
             case (state)
                 IDLE: begin
-                    timerEnable <= 1'b0;
                     obj_location <= 4'b0000;
                     obj_type_out <= 4'b0000;
                     objLoc_r <= 4'b0000;
                     objType_r <= 4'b0000;
                     case (levelNum)
-                        2'd1:begin timerLength <= TIMER_LENGTH1; state <= LEVEL1; timerReconfig <= 1'b1; end
-                        2'd2:begin timerLength <= TIMER_LENGTH2; state <= LEVEL2; timerReconfig <= 1'b1; end
-                        2'd3:begin timerLength <= TIMER_LENGTH3; state <= LEVEL3; timerReconfig <= 1'b1; end
+                        2'd1: state <= LEVEL1;
+                        2'd2: state <= LEVEL2;
+                        2'd3: state <= LEVEL3;
                         default: state <= IDLE;
                     endcase
                 end 
                 LEVEL1: begin
-                    timerEnable <= 1'b1;
-                    timerLength <= TIMER_LENGTH1;
-
                     // moles only 
                     objLoc_r[0] <= object_present[0];
                     objLoc_r[1] <= object_present[1];
@@ -87,28 +75,27 @@ module Levels(levelNum, button_inputs, rng_in, score_increase, score_decrease, m
                     end
                     if (button_inputs[1] & objLoc_r[1] & ~objType_r[1]) begin
                         score_increase <= 1'b1;
-                        objLoc_r[1]   <= 1'b0;
+                        objLoc_r[1] <= 1'b0;
                     end
                     if (button_inputs[2] & objLoc_r[2] & ~objType_r[2]) begin
                         score_increase <= 1'b1;
-                        objLoc_r[2]   <= 1'b0;
+                        objLoc_r[2] <= 1'b0;
                     end
                     if (button_inputs[3] & objLoc_r[3] & ~objType_r[3]) begin
                         score_increase <= 1'b1;
-                        objLoc_r[3]   <= 1'b0;
+                        objLoc_r[3] <= 1'b0;
                     end
                     
-                    if (timerDone) begin
-                        timerEnable <= 1'b0; // stop timer
-                        timerReconfig <= 1'b1; // reconfigure timer for next level
+                    if (timer) begin
+                        obj_location <= 4'b0000;
+                        obj_type_out <= 4'b0000;
+                        objLoc_r <= 4'b0000;
+                        objType_r <= 4'b0000;
                         state <= IDLE; 
                     end
 
                 end
                 LEVEL2: begin
-                    timerEnable <= 1'b1;
-                    timerLength <= TIMER_LENGTH2;
-
                     // moles and spikes (only one type per location)
                     objLoc_r[0] <= obj_present[0];
                     objLoc_r[1] <= obj_present[1];
@@ -124,35 +111,43 @@ module Levels(levelNum, button_inputs, rng_in, score_increase, score_decrease, m
 
                     // hit detection for moles and spikes
                     if (button_inputs[0] & objLoc_r[0]) begin
-                        if (~objType_r[0]) begin score_increase <= 1'b1; end
-                        else               begin score_decrease <= 1'b1; end
-                        objLoc_r[0] <= 1'b0;
+                        if (~objType_r[0]) 
+                            begin score_increase <= 1'b1; end
+                        else               
+                            begin score_decrease <= 1'b1; end
+                            objLoc_r[0] <= 1'b0;
                     end
                     if (button_inputs[1] & objLoc_r[1]) begin
-                        if (~objType_r[1]) begin score_increase <= 1'b1; end
-                        else               begin score_decrease <= 1'b1; end
-                        objLoc_r[1] <= 1'b0;
+                        if (~objType_r[1]) 
+                            begin score_increase <= 1'b1; end
+                        else               
+                            begin score_decrease <= 1'b1; end
+                            objLoc_r[1] <= 1'b0;
                     end
                     if (button_inputs[2] & objLoc_r[2]) begin
-                        if (~objType_r[2]) begin score_increase <= 1'b1; end
-                        else               begin score_decrease <= 1'b1; end
-                        objLoc_r[2] <= 1'b0;
+                        if (~objType_r[2]) 
+                            begin score_increase <= 1'b1; end
+                        else               
+                            begin score_decrease <= 1'b1; end
+                            objLoc_r[2] <= 1'b0;
                     end
                     if (button_inputs[3] & objLoc_r[3]) begin
-                        if (~objType_r[3]) begin score_increase <= 1'b1; end
-                        else               begin score_decrease <= 1'b1; end
-                        objLoc_r[3] <= 1'b0;
+                        if (~objType_r[3]) 
+                            begin score_increase <= 1'b1; end
+                        else               
+                            begin score_decrease <= 1'b1; end
+                            objLoc_r[3] <= 1'b0;
                     end
 
-                    if (timerDone) begin
-                        timerEnable <= 1'b0; // stop timer
-                        timerReconfig <= 1'b1; // reconfigure timer for next level
+                    if (timer) begin
+                        obj_location <= 4'b0000;
+                        obj_type_out <= 4'b0000;        
+                        objLoc_r <= 4'b0000;
+                        objType_r <= 4'b0000;
                         state <= IDLE;
                     end
                 end
                 LEVEL3: begin
-                    timerEnable <= 1'b1;
-                    timerLength <= TIMER_LENGTH3;
 
                     objLoc_r[0] <= obj_present[0];
                     objLoc_r[1] <= obj_present[1];
@@ -188,21 +183,20 @@ module Levels(levelNum, button_inputs, rng_in, score_increase, score_decrease, m
                         objLoc_r[3] <= 1'b0;
                     end
 
-                    if (timerDone) begin
-                        timerEnable <= 1'b0; // stop timer
-                        timerReconfig <= 1'b1; // reconfigure timer for next level
+                    if (timer) begin
+                        obj_location <= 4'b0000;
+                        obj_type_out <= 4'b0000;
+                        objLoc_r <= 4'b0000;
+                        objType_r <= 4'b0000;
                         state <= IDLE;
                     end
                 end
                 default: begin
-                    state <= LEVEL1;
-                    timerEnable <= 1'b0;
-                    timerReconfig <= 1'b0;
-                    timerLength <= TIMER_LENGTH1; 
-                    mole_location <= 4'b0000;
-                    spike_location <= 4'b0000;
-                    moleLoc_r <= 4'b0000;
-                    spikeLoc_r <= 4'b0000;
+                    state <= IDLE;
+                    obj_location <= 4'b0000;
+                    obj_type_out <= 4'b0000;
+                    objLoc_r <= 4'b0000;
+                    objType_r <= 4'b0000;
                 end
             endcase
         end
